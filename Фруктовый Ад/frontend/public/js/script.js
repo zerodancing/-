94b5aafb9708dba858b78f92df31cbb20b3e6082
zerodancing <unamocenyj@gmail.com>
2025-02-07 18:25:51 +0700

    Update script.js

--- Фруктовый Ад/frontend/public/js/script.js
+++ Фруктовый Ад/frontend/public/js/script.js
@@ -66,3 +66,19 @@ fetch('data/products.json')
   .catch(error => {
     console.error('Ошибка при загрузке товаров:', error);
   });
+
+  document.addEventListener("DOMContentLoaded", function () {
+    const menuBtn = document.getElementById("menu-btn");
+    const sidebar = document.getElementById("sidebar");
+
+    menuBtn.addEventListener("click", function () {
+        sidebar.classList.toggle("active"); // Добавляет или убирает класс "active"
+    });
+
+    // Закрытие меню при клике вне его области
+    document.addEventListener("click", function (event) {
+        if (!sidebar.contains(event.target) && !menuBtn.contains(event.target)) {
+            sidebar.classList.remove("active");
+        }
+    });
+});
