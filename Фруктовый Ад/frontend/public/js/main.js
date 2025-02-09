e4dc7527381ad9f252d5627471e41cb894bc2004
zerodancing <unamocenyj@gmail.com>
2025-02-10 05:44:05 +0700

    Update main.js
    меню можно закрыть через крестик

--- Фруктовый Ад/frontend/public/js/main.js
+++ Фруктовый Ад/frontend/public/js/main.js
@@ -123,6 +123,11 @@ document.addEventListener("DOMContentLoaded", function () {
   // Боковое меню
   const menuBtn = document.getElementById("menu-btn");
   const sidebar = document.getElementById("sidebar");
+  const closeMenuBtn = document.querySelector("#sidebar .close-menu");
+
+  closeMenuBtn.addEventListener("click", function () {
+    sidebar.classList.remove("active");
+  });
 
   menuBtn.addEventListener("click", function () {
     sidebar.classList.toggle("active");
